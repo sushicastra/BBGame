324b8ace04e90827a081383ef34b762135a4e1d2
SUSHIBURG <kamiheno.in4ri@gmail.com>
2025-01-24 08:26:41 +0900

    aaaa

--- platformer/scripts/player_modules/animation_module.gd
+++ platformer/scripts/player_modules/animation_module.gd
@@ -2,6 +2,7 @@ class_name AnimationModule
 extends Node
 
 @export var animated_sprite : AnimatedSprite2D
+@export var IDLE_THRESHOLD : float = 10
 
 func handle_animation(body:CharacterBody2D, input:InputModule):
 	var direction = input.get_direction()
@@ -9,7 +10,11 @@ func handle_animation(body:CharacterBody2D, input:InputModule):
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true
-	if abs(body.velocity.x) > 0:
+	
+	if abs(body.velocity.x) > IDLE_THRESHOLD:
 		animated_sprite.play("running")
 	else:
-		animated_sprite.stop()
+		animated_sprite.play("idle")
+	
+	if body.velocity.y < 0:
+		pass
