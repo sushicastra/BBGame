dbccc07c50ca7fd2c673cb941844bf1f053f21ab
SUSHIBURG <kamiheno.in4ri@gmail.com>
2025-01-23 19:20:49 +0900

    aaaa
    aaaa

--- platformer/scripts/player_modules/run_module.gd
+++ platformer/scripts/player_modules/run_module.gd
@@ -4,10 +4,14 @@ extends Node
 @export var SPEED : float = 300
 @export var ACCELERATION : float = 20
 @export var DECELERATION : float = 50
+@export var AIR_ACCELERATION : float = 20
+@export var AIR_DECELERATION : float = 10
 
 func handle_run(body:CharacterBody2D, input:InputModule, delta:float):
 	var direction = input.get_direction()
 	var target_vel = direction * SPEED
 	var vel_diff = target_vel - body.velocity.x
 	var accel_rate = ACCELERATION if abs(direction) > 0 else DECELERATION
+	if not body.is_on_floor():
+		accel_rate = AIR_ACCELERATION if abs(direction) > 0 else AIR_DECELERATION
 	body.velocity.x += vel_diff * accel_rate * delta
