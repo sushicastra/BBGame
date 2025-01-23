621a6867c2dbec9926acfb4c8a9a92b118d6bb93
SUSHIBURG <kamiheno.in4ri@gmail.com>
2025-01-23 23:32:40 +0900

    game update

--- platformer/scripts/player.gd
+++ platformer/scripts/player.gd
@@ -5,9 +5,11 @@ extends CharacterBody2D
 @export var run : RunModule
 @export var jump : JumpModule
 @export var gravity : GravityModule
+@export var animation : AnimationModule
 
 func _physics_process(delta: float) -> void:
 	run.handle_run(self, input, delta)
 	jump.handle_jump(self, input, delta)
 	gravity.handle_gravity(self, input, delta)
+	animation.handle_animation(self, input)
 	move_and_slide()
--- platformer/scripts/player_modules/animation_module.gd
+++ platformer/scripts/player_modules/animation_module.gd
@@ -1,2 +1,15 @@
 class_name AnimationModule
 extends Node
+
+@export var animated_sprite : AnimatedSprite2D
+
+func handle_animation(body:CharacterBody2D, input:InputModule):
+	var direction = input.get_direction()
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	if abs(body.velocity.x) > 0:
+		animated_sprite.play("running")
+	else:
+		animated_sprite.stop()
